93093e0de67bed60b8a4929fe2a779af30622502
Zed Yu <fishheadsam@outlook.com>
2021-03-31 12:55:42 +0100

    change downlaod function to webclient

--- Demo2/MainWindow.xaml.cs
+++ Demo2/MainWindow.xaml.cs
@@ -1,22 +1,20 @@
 ﻿using System.ComponentModel;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Diagnostics;
+using System;
 
 namespace Demo2
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        // TODO: Modify download server a public IP 修改下载地址到公网IP
-        private readonly string ISOfile = "";
-        public int progressPercentage = 0;
-
-
+        public readonly string ISOfileUrl = "https://github.com/fishheadsam/office-in-your-pocket/releases/download/v0.1-alpha/OIP-20.04.2-v0.1.0-desktop-amd64.iso";
 
         public MainWindow()
         {
@@ -24,31 +22,13 @@ namespace Demo2
             InitializeComponent();
         }
 
-        // Progress Bar
-        public event PropertyChangedEventHandler PropertyChanged;
-        private int currentProgress = 0;
-        public int CurrentProgress
-        {
-            get { return currentProgress; }
-            set { 
-                    currentProgress = value; 
-                    OnPropertyChanged("CurrentProgress");
-                }
-        }
-        public void OnPropertyChanged(string PropertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
-        }
-
-        //System.Action growProgress = () =>
-        //{
-        //    CurrentProgress = progressPercentage;
-        //};
-
 
         // Response Student Role button 响应学生角色按钮
-        private async void Download_Student_ISO(object sender, RoutedEventArgs e)
+        private void Download_Student_ISO(object sender, RoutedEventArgs e)
         {
+            string whereWeAre = System.IO.Directory.GetCurrentDirectory();
+            string downloadFileName = whereWeAre + @"\tmp.iso";
+
             firefoxCheck.IsChecked = true;
             teamsCheck.IsChecked = true;
             zoomCheck.IsChecked = true;
@@ -56,12 +36,13 @@ namespace Demo2
             vlcCheck.IsChecked = true;
             onlyofficeCheck.IsChecked = true;
             obsCheck.IsChecked = false;
-            string whereWeAre = System.IO.Directory.GetCurrentDirectory();
-            await Task.Run(() => HttpDownload(ISOfile, whereWeAre));
+
+            status.Content = "Download Start";
+            DownloadFunction(ISOfileUrl, downloadFileName);
         }
 
         // Resoponse Lecture Role button 响应教师角色按钮
-        private async void Download_Lecture_ISO(object sender, RoutedEventArgs e)
+        private void Download_Lecture_ISO(object sender, RoutedEventArgs e)
         {
             firefoxCheck.IsChecked = true;
             teamsCheck.IsChecked = true;
@@ -71,53 +52,66 @@ namespace Demo2
             vlcCheck.IsChecked = true;
             onlyofficeCheck.IsChecked = true;
             string whereWeAre = System.IO.Directory.GetCurrentDirectory();
-            await Task.Run(() => HttpDownload(ISOfile, whereWeAre));
+            string downloadFileName = whereWeAre + @"\tmp.iso";
+
+            DownloadFunction(ISOfileUrl, downloadFileName);
         }
 
-        // Get
 
-
-        // Download Function 下载功能
-        public void HttpDownload(string url, string path)
+        /// <summary>
+        /// 实现下载功能
+        /// </summary>
+        public void DownloadFunction(String http_url, String save_url)
         {
-            int runningByteTotal = 0;
-            string tempPath = System.IO.Path.GetDirectoryName(path) + @"\temp";
-            System.IO.Directory.CreateDirectory(tempPath);
-            string tempFile = tempPath + @"\" + System.IO.Path.GetFileName(path) + ".tmp";
-            if (System.IO.File.Exists(tempFile))
+            if (HttpFileExist(http_url))
             {
-                System.IO.File.Delete(tempFile);
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                    wc.DownloadFileAsync(new System.Uri(http_url), save_url);
+                }
             }
+            else
+            {
+                MessageBox.Show("Download Failed, please try again or send me a email.");
+            }
+        }
+
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            status.Content = (e.ProgressPercentage / progressBar.Maximum) * 100 + "%";
+        }
+
+
+        /// <summary>
+        ///  检查远程文件是否存在 Check file exist
+        /// </summary>
+        private bool HttpFileExist(string http_file_url)
+        {
+            WebResponse response = null;
+            bool result = false;
             try
             {
-                FileStream fs = new FileStream(tempFile, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
-                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-                Stream stream = response.GetResponseStream();
-                byte[] vs = new byte[1024];
-                int size = stream.Read(vs, 0, (int)vs.Length);
-                while (size > 0)
-                {
-                    fs.Write(vs, 0, size);
-                    size = stream.Read(vs, 0, (int)vs.Length);
-                    runningByteTotal += size;
-                    double index = (double)(runningByteTotal);
-                    double total = (double)vs.Length;
-                    double rowProgressPercentage = index / total;
-                    progressPercentage = (int)(rowProgressPercentage * 100);
-                }
-                fs.Close();
-                stream.Close();
-                System.IO.File.Move(tempFile, "demo.iso");
-                // TODO: Add MD5 check function 检查下载的镜像
+                response = WebRequest.Create(http_file_url).GetResponse();
+                result = response == null ? false : true;
             }
-            catch
+            catch (Exception e)
             {
-
+                result = false;
             }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+            return result;
         }
+        
 
-        // Format and Burning function
+        // 调用第三方软件烧录方法 Invoke Format and Burning function
         public bool StartProcess(string runFilePath, params string[] args)
         {
             string s = "";
@@ -134,18 +128,18 @@ namespace Demo2
         }
 
 
-        // Start Burning 
+        // 开始烧录 Start Burning 
         private void startButtonClick(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("");
             string rufus_path = System.IO.Directory.GetCurrentDirectory() + @"\rufus.exe";
-            string iso_path = System.IO.Directory.GetCurrentDirectory() + @"\demo.iso";
+            string iso_path = System.IO.Directory.GetCurrentDirectory() + @"\tmp.iso";
             string[] the_args = { "-i", iso_path};
             StartProcess(rufus_path, the_args);
         }
 
 
-        // Exit function
+        // 退出清理 Exit function
         private void exitButtonClick(object sender, RoutedEventArgs e)
         {
             System.Environment.Exit(0);
